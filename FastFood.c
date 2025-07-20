4b85c1a5c89c9332325bd086d7bf457d8c630d73
Chuck Aballe <chuckaballe60@gmail.com>
2025-07-20 23:36:18 +0800

    New Added codes

--- FastFood.c
+++ FastFood.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+void clearScreen();
 
 typedef struct Order {
     int orderID;
@@ -9,40 +12,64 @@ typedef struct Order {
     float price;
     int quantity;
     float total;
-    char type[10]; 
+    char type[10];
     struct Order* next;
 } Order;
 
 Order* regularHead = NULL;
 Order* seniorHead = NULL;
 
+void pressEnterToContinue() {
+    printf("Press Enter to return to the menu...");
+    getchar(); 
+    getchar();
+}
+
 Order* createOrder() {
     Order* newOrder = (Order*)malloc(sizeof(Order));
+    clearScreen();
     printf("Enter Order ID: ");
     scanf("%d", &newOrder->orderID);
+    getchar(); 
+
     printf("Enter Food: ");
-    scanf("%s", newOrder->food);
+    fgets(newOrder->food, sizeof(newOrder->food), stdin);
+    newOrder->food[strcspn(newOrder->food, "\n")] = '\0'; 
+
     printf("Enter Drink: ");
-    scanf("%s", newOrder->drink);
+    fgets(newOrder->drink, sizeof(newOrder->drink), stdin);
+    newOrder->drink[strcspn(newOrder->drink, "\n")] = '\0';
+
     printf("Enter Price: ");
     scanf("%f", &newOrder->price);
     printf("Enter Quantity: ");
     scanf("%d", &newOrder->quantity);
+
     newOrder->total = newOrder->price * newOrder->quantity;
+    
+    getchar(); 
     printf("Enter Type (regular/senior): ");
-    scanf("%s", newOrder->type);
+    fgets(newOrder->type, sizeof(newOrder->type), stdin);
+    newOrder->type[strcspn(newOrder->type, "\n")] = '\0';
+
+    for (int i = 0; newOrder->type[i]; i++) {
+        newOrder->type[i] = tolower(newOrder->type[i]);
+    }
+
     newOrder->next = NULL;
     return newOrder;
 }
 
 void addOrder() {
     Order* newOrder = createOrder();
+
     if (strcmp(newOrder->type, "regular") == 0) {
         if (regularHead == NULL) {
             regularHead = newOrder;
         } else {
             Order* temp = regularHead;
-            while (temp->next != NULL) temp = temp->next;
+            while (temp->next != NULL)
+                temp = temp->next;
             temp->next = newOrder;
         }
     } else if (strcmp(newOrder->type, "senior") == 0) {
@@ -56,6 +83,7 @@ void addOrder() {
 
 void viewOrders(int option) {
     Order* temp;
+
     if (option == 1) {
         temp = regularHead;
         printf("\n--- REGULAR ORDERS ---\n");
@@ -69,19 +97,22 @@ void viewOrders(int option) {
 
     if (temp == NULL) {
         printf("No orders found.\n");
+        pressEnterToContinue();
         return;
     }
 
     while (temp != NULL) {
-        printf("Order ID: %d\n", temp->orderID);
+        printf("\nOrder ID: %d\n", temp->orderID);
         printf("Food: %s\n", temp->food);
         printf("Drink: %s\n", temp->drink);
         printf("Price: %.2f\n", temp->price);
         printf("Quantity: %d\n", temp->quantity);
         printf("Total: %.2f\n", temp->total);
-        printf("Type: %s\n\n", temp->type);
+        printf("Type: %s\n", temp->type);
         temp = temp->next;
     }
+    printf("\n");
+    pressEnterToContinue();
 }
 
 int deleteOrder(Order** head, int orderID) {
@@ -108,11 +139,13 @@ int deleteOrder(Order** head, int orderID) {
 void payment() {
     int id;
     float amount;
+    clearScreen();
     printf("Enter Order ID to pay: ");
     scanf("%d", &id);
 
     Order* temp = seniorHead;
-    while (temp != NULL && temp->orderID != id) temp = temp->next;
+    while (temp != NULL && temp->orderID != id)
+        temp = temp->next;
 
     if (temp != NULL) {
         printf("Total Amount: %.2f\n", temp->total);
@@ -124,11 +157,13 @@ void payment() {
         } else {
             printf("Insufficient payment.\n");
         }
+        pressEnterToContinue();
         return;
     }
 
     temp = regularHead;
-    while (temp != NULL && temp->orderID != id) temp = temp->next;
+    while (temp != NULL && temp->orderID != id)
+        temp = temp->next;
 
     if (temp != NULL) {
         printf("Total Amount: %.2f\n", temp->total);
@@ -140,28 +175,48 @@ void payment() {
         } else {
             printf("Insufficient payment.\n");
         }
+        pressEnterToContinue();
         return;
     }
 
     printf("Order ID not found.\n");
+    pressEnterToContinue();
+}
+
+void freeOrders(Order* head) {
+    Order* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+void exitL(){
+    clearScreen();
+    printf("    .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .-----------------. .----------------. \n");
+    printf("   | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |\n");
+    printf("   | |  _________   | || |  ____  ____  | || |     _____    | || |  _________   | || |     _____    | || | ____  _____  | || |    ______    | |\n");
+    printf("   | | |_   ___  |  | || | |_  _||_  _| | || |    |_   _|   | || | |  _   _  |  | || |    |_   _|   | || ||_   \\|_   _| | || |  .' ___  |   | |\n");
+    printf("   | |   | |_  \\_|  | || |   \\ \\  / /   | || |      | |     | || | |_/ | | \\_|  | || |      | |     | || |  |   \\ | |   | || | / .'   \\_|   | |\n");
+    printf("   | |   |  _|  _   | || |    > `' <    | || |      | |     | || |     | |      | || |      | |     | || |  | |\\ \\| |   | || | | |    ____  | |\n");
+    printf("   | |  _| |___/ |  | || |  _/ /'`\\ \\_  | || |     _| |_    | || |    _| |_     | || |     _| |_    | || | _| |\\_   |_  | || | \\ `.___]  _| | |\n");
+    printf("   | | |_________|  | || | |____||____| | || |    |_____|   | || |   |_____|    | || |    |_____|   | || ||_____|\____| | || |  `._____.'   | |\n");
+    printf("   | |              | || |              | || |              | || |              | || |              | || |              | || |              | |\n");
+    printf("   | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |\n");
+    printf("   '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  \n");
 }
 
 void menu() {
     int choice;
     do {
+        system("cls || clear");
+        printf(" ______        _    ______              _   _   _       _ \n");
+        printf("|  ___|      | |   |  ___|            | | | | | |     | |\n");
+        printf("| |_ __ _ ___| |_  | |_ ___   ___   __| | | |_| |_   _| |__\n");
+        printf("|  _/ _` / __| __| |  _/ _ \\ / _ \\ / _` | |  _  | | | | '_ \\ \n");
+        printf("| || (_| \\__ \\ |_  | || (_) | (_) | (_| | | | | | |_| | |_) |\n");
+        printf("\\_| \\__,_|___/\\__| \\_| \\___/ \\___/ \\__,_| \\_| |_/\\__,_|_.__/\n");
         
-
-    printf("______        _    ______              _   _   _       _ \n");
-    printf("|  ___|      | |   |  ___|            | | | | | |     | |\n");
-    printf("| |_ __ _ ___| |_  | |_ ___   ___   __| | | |_| |_   _| |__\n");
-    printf("|  _/ _` / __| __| |  _/ _ \\ / _ \\ / _` | |  _  | | | | '_ \\ \n");
-    printf("| || (_| \\__ \\ |_  | || (_) | (_) | (_| | | | | | |_| | |_) |\n");
-    printf("\\_| \\__,_|___/\\__| \\_| \\___/ \\___/ \\__,_| \\_| |_/\\__,_|_.__/ \n");
-    
- 
-
-                                                             
-                                                    
         printf("\n=== FAST FOOD HUB ===\n");
         printf("[1] Add Order\n");
         printf("[2] View Regular Orders\n");
@@ -176,8 +231,10 @@ void menu() {
             case 2: viewOrders(1); break;
             case 3: viewOrders(2); break;
             case 4: payment(); break;
-            case 5: printf("Exiting...\n"); break;
-            default: printf("Invalid option.\n");
+            case 5: exitL(); break;
+            default: 
+                printf("Invalid option.\n");
+                pressEnterToContinue();
         }
 
     } while (choice != 5);
@@ -185,5 +242,14 @@ void menu() {
 
 int main() {
     menu();
+    freeOrders(regularHead);
+    freeOrders(seniorHead);
     return 0;
+}
+void clearScreen(){
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
